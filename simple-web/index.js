c303237bc2c9617082bf6eadea44b9069a2d312e
Pedro Lopez <pjlopezb@gmail.com>
2024-05-28 22:51:09 +0200

    Finish Module 4

--- simple-web/index.js
+++ simple-web/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send("Hello World Mate <3!");
 });
 
 app.listen(4200, () => {
